b8bc3e58fe4d14272c6e4d31d1c8d87e5ae553a0
Martyneqq <cepela.martin12@seznam.cz>
2024-02-21 14:22:14 +0100

    Dithering_new
    version of dithering c# app

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace Dithering
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.trackBar1);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(704, 681);
+            this.splitContainer1.SplitterDistance = 128;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 608);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 19);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Enchance edges";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(16, 655);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 633);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 127;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Random",
+            "Clustered-dot",
+            "Ordered",
+            "Error"});
+            this.comboBox1.Location = new System.Drawing.Point(5, 98);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Decolor";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(246, 655);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(569, 653);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 681);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dithering
+{
+    public partial class Form1 : Form
+    {
+        Bitmap original, remake;
+        Color originalColor, newColor;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ThresholdImage(original, remake);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (original == null)
+                {
+                    throw new ArgumentNullException(paramName: nameof(remake), message: "error");
+                }
+                SaveFileDialog save = new SaveFileDialog();
+                save.Filter = "Image Files|*.jpg;*.jpeg;*.png;";
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(save.FileName);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Load a picture before saving");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opnfd = new OpenFileDialog();
+            opnfd.Filter = "Image Files|*.jpg;*.jpeg;*.png;";
+            if (opnfd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(opnfd.FileName);
+                original = (Bitmap)pictureBox1.Image;
+            }
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                RandomDithering(original, remake);
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                ClusteredDotDithering();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                OrderedDithering();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                ErrorDiffusion(original, remake, 7.0f / 16.0f, 3.0f/16.0f, 5.0f/16.0f, 1.0f/16.0f);
+            }
+        }
+        private Color MakeGray(Color originalColor)
+        {
+            int gray = (byte)(originalColor.R / 3 + originalColor.G / 3 + originalColor.B / 3);
+            originalColor = Color.FromArgb(originalColor.A, gray, gray, gray);
+            return originalColor;
+        }
+        private Color MakeBlack(Color originalColor)
+        {
+            int threshold = trackBar1.Value;
+            int gray = (byte)(originalColor.R / 3 + originalColor.G / 3 + originalColor.B / 3);
+            if (gray < threshold) // Threshold white black
+            {
+                originalColor = Color.FromArgb(originalColor.A, 0, 0, 0);
+
+            }
+            else
+            {
+                originalColor = Color.FromArgb(originalColor.A, 255, 255, 255);
+            }
+            return originalColor;
+        } // Threshold 
+        private Color MakeBlack(Color originalColor, double rnd) // Random
+        {
+            int gray = (byte)(originalColor.R / 3 + originalColor.G / 3 + originalColor.B / 3);
+            if (gray < rnd) // Threshold random values
+            {
+                originalColor = Color.FromArgb(originalColor.A, 0, 0, 0);
+
+            }
+            else
+            {
+                originalColor = Color.FromArgb(originalColor.A, 255, 255, 255);
+            }
+            return originalColor;
+        }
+        private Color MakeBlack(Color originalColor, int value) // Error
+        {
+            int gray = (byte)(originalColor.R / 3 + originalColor.G / 3 + originalColor.B / 3);
+            if (gray < value) // Threshold value
+            {
+                originalColor = Color.FromArgb(originalColor.A, 0, 0, 0);
+            }
+            else
+            {
+                originalColor = Color.FromArgb(originalColor.A, 255, 255, 255);
+            }
+            return originalColor;
+        }
+        private void ThresholdImage(Bitmap original, Bitmap remake)
+        {
+            try
+            {
+                if (original == null)
+                {
+                    throw new ArgumentNullException(paramName: nameof(remake), message: "error");
+                }
+                else
+                {
+                    // Load image
+                    for (int x = 0; x < original.Width; x++)
+                    {
+                        for (int y = 0; y < original.Height; y++)
+                        {
+                            remake = original;
+                            originalColor = original.GetPixel(x, y);
+
+                            //newColor = Color.FromArgb(originalColor.A, gray, gray, gray); // Image is gray until now
+                            newColor = MakeGray(originalColor);
+
+                            remake.SetPixel(x, y, newColor);
+                        }
+                    }
+
+                    // Display
+                    pictureBox1.Image = remake;
+                    remake = (Bitmap)pictureBox1.Image;
+                    // How to save the image?
+                }
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There is an error!");
+            }
+        }
+        private void RandomDithering(Bitmap original, Bitmap remake)
+        {
+            Random rng = new Random();
+            try
+            {
+                if (original==null)
+                {
+                    throw new ArgumentNullException(paramName: nameof(remake), message: "error");
+                }
+                // Loop through the images pixels to reset color.
+                for (int x = 0; x < original.Width; x++)
+                {
+                    for (int y = 0; y < original.Height; y++)
+                    {
+                        remake = original;
+                        double rnd = rng.Next(0, 255);
+                        originalColor = original.GetPixel(x, y);
+                        newColor = MakeBlack(originalColor, rnd);
+                        if (checkBox1.Checked)
+                        {
+                            float newIntensity = EnhancePixel(x, y, remake, newColor);
+                            newColor = Color.FromArgb((byte)newIntensity, (byte)newIntensity, (byte)newIntensity);
+                        }
+                        remake.SetPixel(x, y, newColor);
+                    }
+                }
+
+                pictureBox1.Image = remake;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There is an error in RandomDithering!");
+            }
+        }
+        private void ClusteredDotDithering() // non-functional
+        {
+            int[,] k = { 
+            { 0, 0, 0, 0, 0, 0, 0, 0 }, 
+            { 0, 0, 0, 0, 0, 255, 0, 0 },
+            { 0, 0, 0, 0, 0, 255, 255, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 255, 0, 0, 0, 0, 0, 0 },
+            { 0, 255, 255, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0 }};
+            int n = k.GetLength(0);
+
+            try
+            {
+                if (original == null)
+                {
+                    throw new ArgumentNullException(paramName: nameof(remake), message: "error");
+                }
+                // Loop through the images pixels to reset color.
+                for (int i = 0; i < 63; i++)
+                {
+                    for (int x = 0; x < original.Width; x++)
+                    {
+                        for (int y = 0; y < original.Height; y++)
+                        {
+                            //k[x, y];
+                            remake = original;
+                            int gray = (byte)(originalColor.R / 3 + originalColor.G / 3 + originalColor.B / 3);
+                            originalColor = original.GetPixel(x, y);
+                            Color grayColor = MakeGray(originalColor);
+                            newColor = MakeBlack(originalColor);
+                            original.SetPixel(x, y, newColor);
+
+                            if (gray < 128)
+                            {
+                                newColor = Color.FromArgb(originalColor.A, 0, 0, 0);
+                            }
+                            else
+                            {
+                                newColor = Color.FromArgb(originalColor.A, 255, 255, 255);
+                            }
+                            /*
+                            float errorR = grayColor.R - newColor.R;
+                            float errorG = grayColor.G - newColor.G;
+                            float errorB = grayColor.B - newColor.B;
+
+                            int w = 0;
+                            */
+
+                            remake.SetPixel(x, y, newColor);
+                        }
+                    }
+                }
+
+                pictureBox1.Image = remake;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There is an error in OrderedDithering!");
+            }
+        }
+        private void OrderedDithering()
+        {
+            int threshold = trackBar1.Value;
+            int[,] D = { { 43, 32, 64 },{ 255, 0, 85 },{ 51, 128, 37 } };
+
+            int n = D.GetLength(0);
+
+            try
+            {
+                if (original == null)
+                {
+                    throw new ArgumentNullException(paramName: nameof(remake), message: "error");
+                }
+                // Loop through the images pixels to reset color.
+                for (int x = 0; x < original.Width; x++)
+                {
+                    for (int y = 0; y < original.Height; y++)
+                    {
+                        remake = original;
+                        originalColor = original.GetPixel(x, y);
+                        Color newColor = originalColor;
+                        original.SetPixel(x, y, newColor);
+
+                        float I = newColor.GetBrightness()*255;
+
+                        if (D[x%n, y%n] + threshold < (threshold*2 - I)*(n*n))
+                        {
+                            newColor = Color.FromArgb(originalColor.A, 0, 0, 0);
+                        }
+                        else
+                        {
+                            newColor = Color.FromArgb(originalColor.A, 255, 255, 255);
+                        }
+
+                        remake.SetPixel(x, y, newColor);
+                    }
+                }
+
+                pictureBox1.Image = remake;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There is an error in OrderedDithering!");
+            }
+        }
+        private void ErrorDiffusion(Bitmap original, Bitmap remake, float alpha, float beta, float gamma, float delta)
+        {
+            try
+            {
+                if (original == null)
+                {
+                    throw new ArgumentNullException(paramName: nameof(remake), message: "error");
+                }
+                else
+                {
+                    for (int x = 1; x < original.Width-1; x++)
+                    {
+                        for (int y = 1; y < original.Height-1; y++)
+                        {
+                            remake = original;
+                            originalColor = original.GetPixel(x, y);
+                            Color grayColor = MakeGray(originalColor);
+                            newColor = MakeBlack(grayColor, 128);
+                            original.SetPixel(x, y, newColor);
+
+                            float errorR = grayColor.R - newColor.R;
+                            float errorG = grayColor.G - newColor.G;
+                            float errorB = grayColor.B - newColor.B;
+
+                            newColor = original.GetPixel(x, y + 1);
+                            float r0 = newColor.R + errorR * alpha;
+                            float g0 = newColor.G + errorG * alpha;
+                            float b0 = newColor.B + errorB * alpha;
+                            newColor = Color.FromArgb(grayColor.A, (byte)r0, (byte)g0, (byte)b0);
+                            remake.SetPixel(x, y + 1, newColor);
+
+                            newColor = original.GetPixel(x + 1, y - 1);
+                            float r1 = newColor.R + errorR * beta;
+                            float g1 = newColor.G + errorG * beta;
+                            float b1 = newColor.B + errorB * beta;
+                            newColor = Color.FromArgb(grayColor.A, (byte)r1, (byte)g1, (byte)b1);
+                            remake.SetPixel(x+1, y - 1, newColor);
+
+                            newColor = original.GetPixel(x + 1, y);
+                            float r2 = newColor.R + errorR * gamma;
+                            float g2 = newColor.G + errorG * gamma;
+                            float b2 = newColor.B + errorB * gamma;
+                            newColor = Color.FromArgb(grayColor.A, (byte)r2, (byte)g2, (byte)b2);
+                            remake.SetPixel(x + 1, y, newColor);
+
+                            newColor = original.GetPixel(x + 1, y + 1);
+                            float r3 = newColor.R + errorR * delta;
+                            float g3 = newColor.G + errorG * delta;
+                            float b3 = newColor.B + errorB * delta;
+                            newColor = Color.FromArgb(grayColor.A, (byte)r3, (byte)g3, (byte)b3);
+                            remake.SetPixel(x + 1, y + 1, newColor);
+                        }
+                    }
+                    // Set the PictureBox to display the image.
+                    pictureBox1.Image = remake;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There is an error in ErrorDittering!");
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            /*
+            if (comboBox1.SelectedIndex == 0)
+            {
+                RandomDithering(original, remake);
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                ClusteredDotDithering();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                OrderedDithering();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                ErrorDiffusion(original, remake, 7.0f / 16.0f, 3.0f / 16.0f, 5.0f / 16.0f, 1.0f / 16.0f);
+            }*/
+        }
+        private float AvarageIntensity(int x, int y, Bitmap bitmap, Color color)
+        {
+            int[] offsetX = { 0, -1, 0, 1, 1, 1, 0, -1, -1 };
+            int[] offsetY = { 0, 1, 1, 1, 0, -1, -1, -1, 0 };
+            float result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                int ox = x + offsetX[i];
+                int oy = y + offsetY[i];
+                if (ox > 0 && ox < bitmap.Width && oy > 0 && oy < bitmap.Height)
+                {
+                    color = bitmap.GetPixel(ox, oy);
+                    result += color.GetBrightness() * 255;
+                }
+            }
+            result /= 9;
+            return result;
+        }
+        private float EnhancePixel(int x, int y, Bitmap bitmap, Color color)
+        {
+            int a = 128;
+            color = bitmap.GetPixel(x, y);
+            float intensity = color.GetBrightness() * 255;
+            float avarageIntensity = AvarageIntensity(x, y, bitmap, color);
+
+            float enhancedIntensity = ((a * avarageIntensity) - (intensity))/(255-a);
+            
+            return enhancedIntensity;
+        }
+    }
+}
